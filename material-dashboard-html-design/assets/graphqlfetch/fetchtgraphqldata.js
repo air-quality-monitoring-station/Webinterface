8f5460c50bb8c66e7b43b93ce65ef8199aa621e1
Philipp Strießnig <philippstriessnig@A1.net>
2018-12-03 16:00:54 +0100

    16:00:54-strieszp

--- material-dashboard-html-design/assets/graphqlfetch/fetchtgraphqldata.js
+++ material-dashboard-html-design/assets/graphqlfetch/fetchtgraphqldata.js
@@ -1,3 +1,107 @@
+$(document).ready(function() {
+
+    online = true;
+    infocard = true;
+    var oldpuip;
+    var oldprip;
+    var oldpdnsip;
+    var oldpdnsinf;
+
+    setInterval(function time()
+                {
+
+        //info updater
+        InfoData();
+        re();
+    }, 1000);
+
+});
+
+function re()
+{
+
+    //reload js
+    $("#refresh script").each(function(){
+        var oldScript = this.getAttribute("src");
+        $(this).remove();
+        var newScript;
+        newScript = document.createElement("script");
+        newScript.type = "text/javascript";
+        newScript.src = oldScript;
+        $(newScript).insertAfter( "#iframe" );
+    });
+}
+
+function InfoData()
+{
+
+    //check if there is a connection to the Internet
+    if(!navigator.onLine){
+        alert("There is something wrong!");
+        online = false;
+        InfoOut();
+    }else{
+
+        $.getJSON("https://erebos.xyz/api/graphiql?query=%7BaMessdaten(Datum%3A%222018-11-29%22)%7BUID%2CTemperatur%2CLuftdruck%2CLuftfeuchtigkeit%2CVOC%2CFEINSTAUBPM100%2CEINSTAUBPM25%2CDatumZeit%7D%7D&callback=?", function(data) {
+            //is online
+            online = true;
+
+        }).done(function(json) {
+            //public IPs
+            //try if it is possible to give the preferred info
+            infocard = $('<span class="text-success">').append($('<i class="fa fa-long-arrow-down">').text(json.UID));;
+            InfoOut();
+            online = true;
+        }).fail(function() {
+            /*if (oldpuip === undefined){
+                //altanativ info
+                pipv4s = $("<li>")
+                InfoOut();
+            }else{
+                //some shit is happening
+                InfoOut();
+            }*/
+        }).always(function() {
+            //alert( "complete" );
+        });
+    }
+
+    //local IPs
+
+    //if (lipv4 !== undefined){
+    //    lipv4s = $("<li>").append($('<a>').prop('href','/user/messages').text("Your //local IP addresses: " + lipv4));
+    //    InfoOut();
+    //}
+
+    //IPv6 addresses
+    //if (ipv6 !== undefined){
+    //    ipv6s = $("<li></li>").text("Your IPv6 addresses: " + lipv4);
+    //    $("#ip").append(ipv6s);
+    //}
+
+    //DNS
+    try {
+        jdnsips = $("<li>").addClass("moreinfo").append($('<a>').prop('href','/user/messages').text("Your DNS Ip addresses: " + jdnsip));
+        jdnsinfos = $("<li>").addClass("moreinfo").append($('<a>').prop('href','/user/messages').text("Your DNS Host: " + jdnsinfo));
+        $(".moreinfo-trigger").show();
+        InfoOut();
+    }
+    catch(err){
+
+        InfoOut();
+    }
+}
+
+function InfoOut()
+{
+    //clear ul
+    $(".card-category").empty();
+    //output
+    $(".card-category").append(infocard);
+}
+
+
+/*
 $(function () {
     fetch('https://rrrz55p4vn.lp.gql.zone/graphql', {
         method: 'POST',
@@ -5,7 +109,7 @@ $(function () {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
-        body: JSON.stringify({query: "{  Temperatur, Luftdruck, Luftfeuchtigkeit, VOC, Feinstaub_PM100, Feinstaub_PM25, Timestamp }"})
+        body: JSON.stringify({query: "{  Temperatur, Luftdruck, Luftfeuchtigkeit, VOC, FeinstaubPM100, FeinstaubPM25, Timestamp }"})
     }).then(response => response.json()).then(function(data) {
         Temperatur= data.data.Temperatur
         Luftdruck= data.data.Luftdruck
@@ -18,3 +122,4 @@ $(function () {
 
     alert(Timestamp)
 });
+*/
